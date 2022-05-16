fba5a25f44e98496f4f5d1220d15176cbf055bee
My Name <foo@lisp.us.to>
2022-05-16 16:31:50 -0400

    Making index view csrf-exempt

--- omicron/deploy/views.py
+++ omicron/deploy/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def index(request):
     if request.method == "POST":
         print("Got here")
         return HttpResponse("Did the thing!")
     else:
         return HttpResponse("Post pls")
-
 
 # Create your views here.
