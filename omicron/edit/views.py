49f8f9128f729cb6a2f929cf7df1a4bf45bee649
My Name <foo@lisp.us.to>
2022-05-17 15:44:03 -0400

    Adding a template

--- omicron/edit/views.py
+++ omicron/edit/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! Testing this more!")
+    #return HttpResponse("Hello, world! Testing this more!")
+    return render(request, "edit/main.html", {})
 
 # Create your views here.
