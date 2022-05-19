33b1b0fe6e0a6efbdacf95fa46a6920b43587eda
My Name <foo@lisp.us.to>
2022-05-19 16:07:19 -0400

    Adding an extendable base html template

--- omicron/edit/urls.py
+++ omicron/edit/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name="edit"
+
 urlpatterns = [
-    path('', views.index, name="index")
+    path('',        views.index,  name="index"),
+    path('editor/', views.editor, name="editor")
 ]
--- omicron/edit/views.py
+++ omicron/edit/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def foobar(request):
+    return HttpResponse("Hello, world! Testing this more!")
+
 def index(request):
-    #return HttpResponse("Hello, world! Testing this more!")
     return render(request, "edit/main.html", {})
 
-# Create your views here.
+def editor(request):
+    return render(request, "edit/editor.html", {})
